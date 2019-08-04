57fa309c0ba5a2f17a06d66048a3119002b177ce
Kiyotaka <t10kiyotaka@gmail.com>
2019-08-04 15:34:14 +0900

    Add EmployeeForm component:

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,53 @@
 <template>
   <div id="app" class="small-container">
     <h1>Employees</h1>
-
-    <employee-table />
+    <employee-form @add:employee="addEmployee" />
+    <employee-table　:employees="employees" />
   </div>
 </template>
 
 <script>
 import EmployeeTable from '@/components/EmployeeTable.vue'
+import EmployeeForm from '@/components/EmployeeForm.vue'
 
 export default {
   name: 'app',
   components: {
     EmployeeTable,
+    EmployeeForm,
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Taro Yamada',
+          email: 'taroyamada@sample.com'
+        },
+        {
+          id: 2,
+          name: 'Taro Yamada2',
+          email: 'taroyamada2@sample.com'
+        },
+        {
+          id: 3,
+          name: 'Taro Yamada3',
+          email: 'taroyamada3@sample.com'
+        },
+      ]
+    }
+  },
+  methods: {
+    addEmployee(employee) {
+      const lastId = 
+        this.employees.length > 0
+          ? this.employees[this.employees.length - 1].id
+          : 0;
+      const id = lastId + 1;
+      const newEmployee = { ...employee, id };
+
+      this.employees = [...this.employees, newEmployee];
+    }
   },
 }
 </script>
---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="employee-table">
+    <thead>
+      <tr>
+        <th>Employee name</th>
+        <th>Employee email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="employee in employees" :key="employee.id">
+        <td>{{ employee.name }}</td>
+        <td>{{ employee.email }}</td>
+      </tr>
+    </tbody>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'employee-table',
+  props: {
+    employees: Array,
+  }
+}
+</script>
+
+<style scoped></style>
